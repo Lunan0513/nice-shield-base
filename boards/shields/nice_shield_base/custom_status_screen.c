8c96586b3dbeb4abe514ac7b1736320fe4c30efa
Lunan <981072695@qq.com>
2026-07-21 12:18:29 +0800

    Refactor custom_status_screen.c for clarity
    Updated includes and removed unused code for the status screen.

--- boards/shields/nice_shield_base/custom_status_screen.c
+++ boards/shields/nice_shield_base/custom_status_screen.c
@@ -1,23 +1,16 @@
-#include "widgets/screen.h"
+#include <lvgl.h>
+#include <zmk/display.h>
 
-#include <zephyr/logging/log.h>
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
-
-#include "assets/pixel_operator_mono.c"
-#include "assets/custom_fonts.h"
-
-#if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
-static struct zmk_widget_screen screen_widget;
-#endif
+// 声明外部变量（定义在 assets 文件夹里）
+extern const lv_img_dsc_t left_image;
+extern const lv_img_dsc_t right_image;
 
 lv_obj_t *zmk_display_status_screen() {
-    lv_obj_t *screen;
-    screen = lv_obj_create(NULL);
+    lv_obj_t *screen = lv_obj_create(NULL);
 
-#if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
-    zmk_widget_screen_init(&screen_widget, screen);
-    lv_obj_align(zmk_widget_screen_obj(&screen_widget), LV_ALIGN_TOP_LEFT, 0, 0);
-#endif
+    lv_obj_t *img = lv_img_create(screen);
+    lv_img_set_src(img, &left_image);
+    lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
 
     return screen;
 }
